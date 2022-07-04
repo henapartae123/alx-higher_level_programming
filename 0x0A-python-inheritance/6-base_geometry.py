244193f6915259f89ecd5a236ac0ba8ee6ddc509
henapaartae123 <henyvibe@gmail.com>
2022-07-04 22:12:55 +0300

    inherit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+BaseGeometry
+"""
+
+class BaseGeometry:
+    """find area
+    """
+    def area(self):
+        """find area
+
+        Raises:
+            Exception: area is not implemented
+
+        """
+
+        raise Exception("area() is not implemented")

ff4b3400bfd35374983a46b657936e3ae1a648c0
henapaartae123 <henyvibe@gmail.com>
2022-07-04 21:27:50 +0300

    inherit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,12 +4,18 @@ Returns a list object
 """
 
 class MyList(list):
-    """Custom List
-    """
+
+    """a subclass of list"""
+    
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
+    """sorted List"""
     def print_sorted(self):
         """
         Returns: 
             prints the list, but sorted (ascending sort)
-            
+
         """
         print(sorted(self))

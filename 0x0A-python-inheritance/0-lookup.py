d17af7c441a90ddc0bdbe702614fba795489d0e5
henapaartae123 <henyvibe@gmail.com>
2022-07-04 20:56:12 +0300

    inherit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""
+Returns the list of available attributes and methods of an object:
+"""
+
 def lookup(obj):
-    """Returns a list object."""
+    """Returns a list object.
+
+    Args:
+        obj : ovject
+
+    Returns:
+        list: list of attributes and members
+        
+    """
     return (dir(obj))

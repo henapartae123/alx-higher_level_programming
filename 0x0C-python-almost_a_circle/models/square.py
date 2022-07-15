858dad971d01dc9a90fd0df090b1718fb6062473
henapaartae123 <henyvibe@gmail.com>
2022-07-15 15:02:31 +0300

    model

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a square class."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Init the Square"""
+        
+        super().__init__(size, size, x, y, id)

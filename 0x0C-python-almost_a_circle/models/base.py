ddae4c1d86dcdb69c452b600f0cdf6c032715eb5
henapaartae123 <henyvibe@gmail.com>
2022-07-15 15:23:21 +0300

    model

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''Base class module'''
+from json import dumps
 
 class Base:
     '''Base class'''
@@ -15,4 +16,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-            
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''json representation of the list of dictionaries'''
+
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)

d39d40aca56a280256318cdeddb114f002d6d18f
henapaartae123 <henyvibe@gmail.com>
2022-07-15 16:14:20 +0300

    model

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -45,3 +45,19 @@ class Base:
                 text.append(lst.to_dictionary())
         with open(filename, mode="w", encoding="utf-8") as f:
             return f.write(Base.to_json_string(text))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """create an instance with all attributes"""
+
+        from models.rectangle import Rectangle
+        from models.square import Square
+        
+        if cls is Rectangle:
+            new = Rectangle(1, 1)
+        elif cls is Square:
+            new = Square(1)
+        else:
+            new = None
+        new.update(**dictionary)
+        return new

3a77a722dfa2230ea4bbdb8387b1f5907212e28c
henapaartae123 <henyvibe@gmail.com>
2022-07-15 10:40:55 +0300

    model

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+'''Rectangle class module - tests located in tests/test_base.py'''
+from models.base import Base
+
+
+class Rectangle(Base):
+    '''Rectangle class'''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+
+        '''init rectangle'''
+
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        '''get width of a rectangle'''
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''set width of a rectangle'''
+        
+        self.validate_integer("width", value, False)
+        self.__width = value
+
+    @property
+    def height(self):
+        '''get height of a rectangle'''
+        
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''set height of a rectangle'''
+        
+        self.validate_integer("height", value, False)
+        self.__height = value
+
+    @property
+    def x(self):
+        '''get x of a rectangle'''
+        
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        '''set x of a rectangle'''
+        
+        self.validate_integer("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        '''get y of a rectangle'''
+        
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        '''set y of a rectangle'''
+        
+        self.validate_integer("y", value)
+        self.__y = value
+        

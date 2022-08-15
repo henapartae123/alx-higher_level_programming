86510001317647d224b744ba2f08b744017cf8ed
henapaartae123 <henyvibe@gmail.com>
2022-08-15 21:52:48 +0300

    sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- lists all cities in the databse hbtn_0d_usa in ascending order
+SELECT c.`id`, c.`name`, s.`name`
+ FROM `cities` AS cities
+       INNER JOIN `states` AS set
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;

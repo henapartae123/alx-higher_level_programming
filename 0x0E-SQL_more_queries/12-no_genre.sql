49d00e8c84f4d7312e46a6c301e68a54e196d124
henapaartae123 <henyvibe@gmail.com>
2022-08-15 22:28:59 +0300

    sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- lists all shows wothout a genre linked
+SELECT shows.`title`, genres.`genre_id`
+  FROM `tv_shows` AS shows
+       LEFT JOIN `tv_show_genres` AS genres
+       ON shows.`id` = genres.`show_id`
+       WHERE genres.`genre_id` IS NULL
+ ORDER BY shows.`title`, genres.`genre_id`;
+ 

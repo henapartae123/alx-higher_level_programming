cd4364f7bd99c40ab40a9559ec2d3335a76a5ad5
henapaartae123 <henyvibe@gmail.com>
2022-09-05 15:06:08 +0300

    js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = ["C is fun", "Python is cool", "JavaScript is amazing"];
-console.log("array"[0]);
-console.log("array"[1]);
-console.log("array"[2]);
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);

66594092d0dc7ef13103ce664fdf8f3515d97b56
henapaartae123 <henyvibe@gmail.com>
2022-09-07 10:46:51 +0300

    js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,6 +2,8 @@
 
 module.exports = class Rectangle {
   constructor(w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    if (w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
+    }
   }
 };
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,10 +2,12 @@
 
 module.exports = class Rectangle {
   constructor(w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    if (w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
+    }
   }
 
   print() {
-    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+    for (let i = 0; i < this.height; i++) console.log("X".repeat(this.width));
   }
 };

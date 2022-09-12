e84773b6d4e3a33dd9ea58486d97a4f7261ac460
henapaartae123 <henyvibe@gmail.com>
2022-09-12 10:07:29 +0300

    py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Defines a class MyInt that inherits from int
+"""
+
+
+class MyInt(int):
+    """Invert int operators == and !=."""
+
+    def __eq__(self, value):
+        
+        """
+        Args:
+            value (int): value
+
+        """
+
+        return self.real != value
+
+    def __ne__(self, value):
+        
+        """
+        Args:
+            value (int): value
+            
+        """
+        
+        return self.real == value

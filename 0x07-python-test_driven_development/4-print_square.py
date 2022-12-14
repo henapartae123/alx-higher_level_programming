e8ca3442624f257bffbc3117c114c6e4fb7e5006
Henok Shewakena <henyvibe@gmail.com>
2022-12-14 10:04:32 +0300

    test

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -13,11 +13,9 @@ def print_square(size):
         ValueError: size must be >= 0
 
     """
-    if not isinstance(size, int):
+    if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-
-    for i in range(size):
-        [print("#", end="") for j in range(size)]
-        print("")
+    if size > 0:
+        print(("#" * size + "\n") * size, end="")

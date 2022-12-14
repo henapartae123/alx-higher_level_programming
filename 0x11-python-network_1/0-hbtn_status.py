6332c1e4f57a2780f815f05a2bdcf7dee0323602
unknown <henyvibe@gmail.com>
2022-12-14 21:24:11 +0300

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
-script that fetches https://alx-intranet.hbtn.io/status
-use the package urllib
+    A python script that fetches URL using 'urlib'
 """
-import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
 
-print('Body response:\n\t- type: {}'.format(type(html)))
-print('\t- content: {}'.format(html))
-print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+if __name__ == "__main__":
+    from urllib.request import urlopen
+
+    with urlopen("https://alx-intranet.hbtn.io/status") as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "strict")))

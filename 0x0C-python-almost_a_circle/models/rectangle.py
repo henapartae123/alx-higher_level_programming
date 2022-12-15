1b2851b01acd96b465d74572dd6f3a0f0609c22f
Henok Shewakena <henyvibe@gmail.com>
2022-12-15 22:56:49 +0300

    csv

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,7 +7,6 @@ class Rectangle(Base):
     '''Rectangle class'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-
         '''init rectangle'''
 
         super().__init__(id)
@@ -25,46 +24,46 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         '''set width of the rectangle'''
-        
+
         self.validate_integer("width", value, False)
         self.__width = value
 
     @property
     def height(self):
         '''get height of the rectangle'''
-        
+
         return self.__height
 
     @height.setter
     def height(self, value):
         '''set height of the rectangle'''
-        
+
         self.validate_integer("height", value, False)
         self.__height = value
 
     @property
     def x(self):
         '''get x of the rectangle'''
-        
+
         return self.__x
 
     @x.setter
     def x(self, value):
         '''set x of the rectangle'''
-        
+
         self.validate_integer("x", value)
         self.__x = value
 
     @property
     def y(self):
         '''get y of the rectangle'''
-        
+
         return self.__y
 
     @y.setter
     def y(self, value):
         '''set y of a rectangle'''
-        
+
         self.validate_integer("y", value)
         self.__y = value
 
@@ -77,24 +76,24 @@ class Rectangle(Base):
             raise ValueError("{} must be >= 0".format(name))
         elif not isequal and value <= 0:
             raise ValueError("{} must be > 0".format(name))
-    
+
     def area(self):
         '''area of the rectangle'''
-        
+
         return self.width * self.height
 
     def display(self):
         '''print stdout of the rectangle'''
 
         display = '\n' * self.y + \
-              (' ' * self.x + '#' * self.width + '\n') * self.height
+            (' ' * self.x + '#' * self.width + '\n') * self.height
         print(display, end='')
 
     def __str__(self):
         '''str info about a rectangle'''
         return '[Rectangle] ({}) {}/{} - {}/{}'. \
-            format(self.id, self.x, self.y, self.width,self.height)
-            
+            format(self.id, self.x, self.y, self.width, self.height)
+
     def __update(self, id=None, width=None, height=None, x=None, y=None):
         '''update instance attributes via */**args'''
         if id is not None:
@@ -107,17 +106,16 @@ class Rectangle(Base):
             self.x = x
         if y is not None:
             self.y = y
-    
+
     def update(self, *args, **kwargs):
         '''update instance attributes via */** args'''
         if args:
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
-    
+
     def to_dictionary(self):
         '''dictionary representation of the class'''
 
         return {"id": self.id, "width": self.__width, "height": self.__height,
                 "x": self.__x, "y": self.__y}
-                

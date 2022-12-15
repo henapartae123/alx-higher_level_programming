333e6754a670d6850b6c4a8a004b57ebe693114e
Henok Shewakena <henyvibe@gmail.com>
2022-12-15 23:21:44 +0300

    hidden

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
-
 if __name__ == "__main__":
-    arg_num = len(argv) - 1
-    if arg_num == 0:
-        print("{}".format(arg_num))
-    else:
-        result = []
-        for i in range(1, arg_num + 1):
-            result.append(int(argv[i]))
-        print("{}".format(sum(result)))
+    from sys import argv
+    num_args = len(argv)
+    kim = 0
+    for k in range(1, num_args):
+        kim += int(argv[k])
+    print("{:d}".format(kim))

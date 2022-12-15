32f11a4a11d7cd3608e5c9e722b06577e0b3ee80
Henok Shewakena <henyvibe@gmail.com>
2022-12-15 09:04:44 +0300

    status

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     import urllib.parse as parse
     import urllib.request as request
     from sys import argv
+
     values = {'email': argv[2]}
     data = parse.urlencode(values).encode('utf-8')
     req = request.Request(argv[1], data)
